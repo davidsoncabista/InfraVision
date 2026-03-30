dfa3e5c5362e9d6c887ca03bc04122347662e84a
davidson Santos Conceição <davidson.cabista@gmail.com>
2026-03-30 12:11:57 -0300

    fix: redirect login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,5 +4,5 @@ import { redirect } from 'next/navigation';
 // Esta página agora atua como um redirecionamento para a nova página /login1
 // para contornar problemas persistentes de cache.
 export default function LoginPage() {
-  redirect('/login1');
+  redirect('/login');
 }
